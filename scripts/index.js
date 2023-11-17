1b54f0d18f2f0903d34810d431c0fd0abac56d5f
selormbutsorgamenyah <selormbutsorgamenyah@protonmail.com>
2023-11-17 14:58:17 +0000

    Header, Navigation, Hero for Homepage

---
+++ scripts/index.js
@@ -0,0 +1,42 @@
+(() => {
+    const openNav = document.querySelector(".open-menu"),
+    closeNav = document.querySelector(".close-menu"),
+    navMenu = document.querySelector(".nav-links-container"),
+    mediaSize = 992;
+
+    openNav.addEventListener("click", toggleMenu);
+    closeNav.addEventListener("click", toggleMenu);
+
+    function toggleMenu() {
+        navMenu.classList.toggle("open");
+    }
+
+    navMenu.addEventListener("click", (event) => {
+        if (
+            event.target.hasAttribute("data-toggle") && 
+            window.innerWidth < mediaSize
+        ) {
+            event.preventDefault();
+            const dropdownMenuBranch = event.target.parentElement;
+
+            if (dropdownMenuBranch.classList.contains("active")) {
+                collapseDropdownMenu();
+            } else {
+                if (navMenu.querySelector(".dropdown-menu-branch.active")) {
+                    collapseDropdownMenu();
+                }
+
+                dropdownMenuBranch.classList.add("active");
+                const dropdownMenu = dropdownMenuBranch.querySelector(".dropdown-menu");
+                dropdownMenu.style.maxHeight = dropdownMenu.scrollHeight + "px";
+            }
+        }
+    });
+
+    function collapseDropdownMenu() {
+        navMenu.querySelector(".dropdown-menu-branch.active .dropdown-menu").removeAttribute("style")
+        navMenu.querySelector(".dropdown-menu-branch.active").classList.remove("active")
+    }
+
+    
+})();
